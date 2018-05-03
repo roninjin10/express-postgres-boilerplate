a09d7c5294e6319eeb6ed5cfd98a340bf6417cf9
Kyle Shifflett <kshifflett707@gmail.com>
2018-05-02 19:28:18 -0700

    Added database boilerplate

---
+++ db/example.js
@@ -0,0 +1,20 @@
+router.get('/', function(req, res, next) {
+    Product.findAll({
+            include: [Review]
+        })
+        .then(result => {
+            res.status(200).send(result);
+        })
+        .catch(next);
+});
+
+router.get('/:id', function(req, res, next) {
+    Product.findOne({
+            where:{id:req.params.id},
+            include: [Review]
+        })
+        .then(result => {
+            res.status(200).send(result);
+        })
+        .catch(next);
+});
