22e06d00d09e62b624fbacc332b221bc384288c6
Frederick Peavy <peavy.frederick@gmail.com>
2018-05-04 23:03:58 -0400

    Add routes and controllers for sign-in, sign-up, questions and answers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import applyMiddleware from './middleware'
-import controller from './controller'
+// import controller from './controller/questions'
+import router from './router'
 
 const app = express();
 
 applyMiddleware(app);
 
-app.use(controller);
+app.use(router);
 
 export default app;
---
+++ src/controller/questions.js
@@ -0,0 +1,27 @@
+let questionsController = {
+    get: {},
+    post: {},
+    patch: {}
+};
+
+questionsController.get.commentsByQuestionId = (req, res) => {
+    res.sendStatus(200).end()
+};
+
+questionsController.get.answersByQuestionId = (req, res) => {
+    res.sendStatus(200).end()
+}
+
+questionsController.get.questionByQUestionId = (req, res) => {
+    res.sendStatus(200).end()
+}
+
+questionsController.get.questionsByUserId = (req, res) => {
+    res.sendStatus(200).end()
+}
+
+questionsController.get.allQuestions = (req, res) => {
+    res.sendStatus(200).end()
+}
+
+export default questionsController
